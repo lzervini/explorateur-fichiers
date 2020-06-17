7c58814f69d677fce4887897b373000900b34b44
lzervini <lezervinia@gmail.com>
2020-06-17 16:13:10 +0200

    Add all files

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php require 'header.php'; ?>
+
+<?php
+$home = "home";
+if(!is_dir($home)){
+    mkdir("home");
+}
+
+if(!isset($_POST['cwd'])){
+   $url = getcwd() . DIRECTORY_SEPARATOR . $home;  
+}
+else{
+    $url = $_POST['cwd'];
+}
+chdir($url );
+$content = scandir($url);
+//print_r($content);
+//Etape 4
+$path = "";
+$breadcrumbs = explode(DIRECTORY_SEPARATOR,$url);
+  echo "<form method='post' id='change_cwd'>";  
+foreach($breadcrumbs as $item){
+        $path .= $item.DIRECTORY_SEPARATOR;
+        echo "<button type='submit' value='".substr($path,0,-1)."' name='cwd'>";
+        echo $item; 
+        echo "</button>";
+
+    }
+echo "</form>";
+$contents = [];
+foreach ($content as $item) {
+    if ($item !== "." && $item !== "..") {
+        echo"<button type='submit' form='change_cwd' value='".$url.DIRECTORY_SEPARATOR.$item."' name='cwd'>";
+        echo "<br>".$item ;
+        echo "</button>";
+      $contents[$item] = $item;
+    }
+  }
+
+?>
