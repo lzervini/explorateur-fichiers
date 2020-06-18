c23192ee11ec5772c84c8e1991945f5d9ae989f4
lzervini <lezervinia@gmail.com>
2020-06-18 14:46:57 +0200

    Resolution d'un probleme

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require 'header.php'; ?>
+<?php require_once "header.php" ?>
 
 <?php
 $home = "home";
@@ -14,28 +14,27 @@ else{
 }
 chdir($url );
 $content = scandir($url);
-//print_r($content);
-//Etape 4
+// print_r($content);
 $path = "";
 $breadcrumbs = explode(DIRECTORY_SEPARATOR,$url);
-  echo "<form method='post' id='change_cwd'>";  
+  echo "<form method='post' id='ch_cwd'>";  
 foreach($breadcrumbs as $item){
+  if ($item !== ""){
         $path .= $item.DIRECTORY_SEPARATOR;
         echo "<button type='submit' value='".substr($path,0,-1)."' name='cwd'>";
         echo $item; 
         echo "</button>";
-
-
     }
-echo "</form>";
+  }
+    echo "</form>";
 $contents = [];
 foreach ($content as $item) {
     if ($item !== "." && $item !== "..") {
-        echo"<button type='submit' form='change_cwd' value='".$url.DIRECTORY_SEPARATOR.$item."' name='cwd'>";
-        echo "<br>".$item ;
-        echo "</button>";
-      $contents[$item] = $item;
+            echo "<br><button type='submit' form='ch_cwd' value='".$url.DIRECTORY_SEPARATOR.$item."' name='cwd'>";
+            echo $item ;
+            echo "</button>";
+     // $contents[$item] = $item;
     }
   }
-
+ 
 ?>
